0bc75e3cedf162e8861fd0b08292856763a5c428
chelakuty <raghavan8007@gmail.com>
2020-03-20 14:41:05 +0530

    lets try till it get success

--- Library/Lib.bat
+++ Library/Lib.bat
@@ -1,5 +1,5 @@
 pip install selenium
 
 "C:\Users\ssrivathsan\AppData\Local\Programs\Python\Python36\Python.exe"
-"C:\Users\ssrivathsan\AppData\Local\Programs\Python\Python36\FirstProjectjenkins\venv\Scripts\chromedriver.exe"
+
 "C:\Users\ssrivathsan\AppData\Local\Programs\Python\Python36\FirstProjectjenkins\Test.py"
