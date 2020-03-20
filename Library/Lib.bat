2a29e5de02a6d6539c7ab3a8568e9cec5d3c3152
chelakuty <raghavan8007@gmail.com>
2020-03-20 12:10:26 +0530

    changed the lib files

--- Library/Lib.bat
+++ Library/Lib.bat
@@ -1,4 +1,5 @@
 pip install selenium
 
-"C:\Users\ssrivathsan\AppData\Local\Programs\Python\Python36\FirstProjectjenkins\Test.py"
+
 "C:\Users\ssrivathsan\AppData\Local\Programs\Python\Python36\FirstProjectjenkins\venv\Scripts\python.exe"
+"C:\Users\ssrivathsan\AppData\Local\Programs\Python\Python36\FirstProjectjenkins\Test.py"
